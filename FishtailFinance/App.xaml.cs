1ba98a64f2dcd8b6f5ec358ef87fad2afa8a74d7
Yubrajgg <125936559+Yubrajgg@users.noreply.github.com>
2024-12-19 08:50:45 +0545

    Add project files.

---
+++ FishtailFinance/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FishtailFinance
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FishtailFinance/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FishtailFinance
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
